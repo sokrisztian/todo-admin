5272a88191e7076dd92af711d08cbd659c8f292c
Krisztian Somogyi <sokrisztian@gmail.com>
2020-06-11 22:45:46 +0200

    Added delete todo FE

--- src/main/java/sokrisztian/todo/admin/api/controller/DeleteTodoController.java
+++ src/main/java/sokrisztian/todo/admin/api/controller/DeleteTodoController.java
@@ -2,14 +2,13 @@ package sokrisztian.todo.admin.api.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import sokrisztian.todo.admin.logic.service.DeleteTodoService;
 
-@RestController
-@RequestMapping("/todos")
+@Controller
 public class DeleteTodoController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DeleteTodoController.class);
@@ -20,11 +19,12 @@ public class DeleteTodoController {
         this.service = service;
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable int id) {
+    @PostMapping("/todos/{id}/delete")
+    public String deleteById(@PathVariable int id, @RequestHeader String referer) {
         LOGGER.info("Delete TODO request with {} TODO ID arrived", id);
         service.deleteById(id);
         LOGGER.info("Delete TODO request with {} TODO ID served", id);
+        return "redirect:" + referer;
     }
 
 }
