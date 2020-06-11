13f2010fe4a56d5a7923fd4bdaa6e5d27078e056
Krisztian Somogyi <sokrisztian@gmail.com>
2020-06-11 19:05:13 +0200

    Added show todos FE

--- src/main/java/sokrisztian/todo/admin/api/controller/FindTodosByUserIdController.java
+++ src/main/java/sokrisztian/todo/admin/api/controller/FindTodosByUserIdController.java
@@ -1,16 +1,15 @@
 package sokrisztian.todo.admin.api.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import sokrisztian.todo.admin.api.model.TodoView;
 import sokrisztian.todo.admin.logic.service.FindTodosByUserIdService;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/todos")
+@Controller
 public class FindTodosByUserIdController {
 
     private final FindTodosByUserIdService service;
@@ -19,9 +18,13 @@ public class FindTodosByUserIdController {
         this.service = service;
     }
 
-    @GetMapping
-    public List<TodoView> findByUserId(@RequestParam int userId) {
+    @ModelAttribute("todos")
+    public List<TodoView> addTodos(@RequestParam int userId) {
         return service.findByUserId(userId);
+    }
+
+    @GetMapping("/todos")
+    public void showTodos() {
     }
 
 }
