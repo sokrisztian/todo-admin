f9b4459802e3c0646e4a426444ab36182d925695
Krisztian Somogyi <sokrisztian@gmail.com>
2020-06-12 01:32:12 +0200

    Added upload avatar FE

--- src/main/java/sokrisztian/todo/admin/api/controller/UploadAvatarController.java
+++ src/main/java/sokrisztian/todo/admin/api/controller/UploadAvatarController.java
@@ -2,12 +2,15 @@ package sokrisztian.todo.admin.api.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 import sokrisztian.todo.admin.logic.service.UploadAvatarService;
 
-@RestController
-@RequestMapping("/users")
+@Controller
 public class UploadAvatarController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UploadAvatarController.class);
@@ -18,11 +21,22 @@ public class UploadAvatarController {
         this.service = service;
     }
 
-    @PostMapping("/{id}")
-    public void upload(@PathVariable int id, @RequestBody MultipartFile avatarImage) {
+    @ModelAttribute("userId")
+    public int addUserId(@PathVariable int id) {
+        return id;
+    }
+
+    @GetMapping("/users/{id}/upload-avatar")
+    public String upload() {
+        return "uploadAvatar";
+    }
+
+    @PostMapping("/users/{id}/upload-avatar")
+    public String upload(@PathVariable int id, @ModelAttribute MultipartFile avatarImage) {
         LOGGER.info("Upload avatar request with {} user ID arrived", id);
         service.upload(id, avatarImage);
         LOGGER.info("Upload avatar request with {} user ID served", id);
+        return "redirect:/users";
     }
 
 }
